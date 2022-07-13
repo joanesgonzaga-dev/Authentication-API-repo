ba9bc72a931d18510e2e0ed0b8e1e449beb08ab6
Joanes Gonzaga <joanesgonzaga.dev@gmail.com>
2022-07-12 22:31:22 -0300

    Commit after Swagger Doc and Testing middleware

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Softaway.Identidade.API.Data;
+using System;
 
 namespace Softaway.Identidade.API
 {
@@ -30,10 +32,27 @@ namespace Softaway.Identidade.API
                 .AddDefaultTokenProviders(); //Nao tem a ver com o JWT
 
             services.AddControllers();
+
+            //Adds the Documentation generator middleware
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("V1", new OpenApiInfo 
+                {
+                    Title = "Mobster91 Enterprise Identity API",
+                    Description = "Authentication Control Layer",
+                    Contact = new OpenApiContact { Name = "Joanes Gonzaga", Email = "joanesgonzaga-dev@gmail.com"},
+                    License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://www.opensource.org/licenses/mit")}
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/V1/swagger.json", "V1");
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
